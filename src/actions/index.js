ed334e66f2fb2b8077764eac0e79a793aa8f14fc
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-01-17 15:51:41 -0300

    Feat: action playerLogin

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,8 +5,12 @@ const receiveMovies = (token) => ({
   token,
 });
 
-export default function fetchMovies() {
+export function fetchMovies() {
   return (dispatch) => fetch('https://opentdb.com/api_token.php?command=request')
     .then((response) => response.json())
     .then((token) => dispatch(receiveMovies(token)));
 }
+
+export const PLAYER_LOGIN = 'PLAYER_LOGIN';
+
+export const playerLogin = (name, hash) => ({ type: PLAYER_LOGIN, name, hash });
